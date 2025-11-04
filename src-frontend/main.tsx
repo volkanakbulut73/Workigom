280720b4a09103e5a33d3cb93e33925fe000d57b
volkanakbulut73 <volkanbulut73@gmail.com>
2025-11-05 01:17:49 +0300

    Update main.tsx

--- src-frontend/main.tsx
+++ src-frontend/main.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext"; // AuthContext burada olmalı
+import { AuthProvider } from "./contexts/AuthContext"; 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
