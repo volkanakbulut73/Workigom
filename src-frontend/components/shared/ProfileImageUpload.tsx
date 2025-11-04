f2d4a213167928199ddc3c49828b0563d546b04e
volkanakbulut73 <volkanbulut73@gmail.com>
2025-11-05 01:19:18 +0300

    Update ProfileImageUpload.tsx

--- src-frontend/components/shared/ProfileImageUpload.tsx
+++ src-frontend/components/shared/ProfileImageUpload.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "../../contexts/AuthContext";
 import { supabase } from "../../lib/supabaseClient";
 import { toast } from "sonner";
 
