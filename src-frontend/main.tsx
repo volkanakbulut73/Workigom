7af6397af58599bafb1e58ae642db050844dbfd8
volkanakbulut73 <volkanbulut73@gmail.com>
2025-11-05 01:07:20 +0300

    Update main.tsx

--- src-frontend/main.tsx
+++ src-frontend/main.tsx
@@ -1,27 +1,14 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+// Workigom/src-frontend/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext"; // AuthContext burada olmalı
 import "./index.css";
-import {
-  AuthProvider,
-  JobsProvider,
-  ApplicationsProvider,
-  DonationsProvider,
-  MessagesProvider,
-  NotificationsProvider,
-} from "./contexts";
 
-createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <JobsProvider>
-      <ApplicationsProvider>
-        <DonationsProvider>
-          <MessagesProvider>
-            <NotificationsProvider>
-              <App />
-            </NotificationsProvider>
-          </MessagesProvider>
-        </DonationsProvider>
-      </ApplicationsProvider>
-    </JobsProvider>
-  </AuthProvider>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
